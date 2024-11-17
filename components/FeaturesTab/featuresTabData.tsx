6c3a1cd58af80b0be5c7ba32bc16e0a707878ccc
Sidney Gathrid <sgathrid@yahoo.com>
2024-11-17 12:16:24 -0800

    adding krv images

--- components/FeaturesTab/featuresTabData.tsx
+++ components/FeaturesTab/featuresTabData.tsx
@@ -6,7 +6,7 @@ const featuresTabData: FeatureTab[] = [
     title: "LLMs Have Limits",
     desc1: `LLMs, RAG, and predictive ML tools are transforming industries, but they struggle with small, niche, sparse, high-dimensional, and noisy datasets — typical of real-world data, especially for smaller organizations. Industry professionals can't get quality analytics by simply uploading a local CSV to ChatGPT.`,
     desc2: `    With Krv, now they can. Tabular knowledge graphs bring the accessibility, sophistication, and interpretability of LLMs and RAG to high dimensional, sparse datasets.`,
-    image: "/images/features/features-light-01.png",
+    image: "/images/features/features-light-01.svg",
     imageDark: "/images/features/features-dark-01.svg",
   },
   {
@@ -15,7 +15,7 @@ const featuresTabData: FeatureTab[] = [
       "Effortless, Sophisticated, and Reliable Data Preprocessing and Exploration for SME's",
     desc1: `Unlike traditional solutions that require deep technical expertise or large data teams, our tool empowers SMEs to use cutting-edge machine learning techniques with minimal setup.`,
     desc2: `    No PhD in Data Science Required.`,
-    image: "/images/features/features-light-01.png",
+    image: "/images/features/features-light-01.svg",
     imageDark: "/images/features/features-dark-01.svg",
   },
   {
@@ -23,7 +23,7 @@ const featuresTabData: FeatureTab[] = [
     title: "The Curse of Dimensionality is Knocking at our Door",
     desc1: `Effective data cleaning, preprocessing, and representation are crucial for extracting insights, especially with small, industry-specific datasets. While research offers many methods, not everyone has the expertise to navigate them. With so many options, making reliable choices is a challenge — even for data scientists.`,
     desc2: `Our tools automate the machine-learning data handling for you. Save time and understand the trustworthiness of your data's ability to address your end goal.`,
-    image: "/images/features/features-light-01.png",
+    image: "/images/features/features-light-01.svg",
     imageDark: "/images/features/features-dark-01.svg",
   },
 ];
